f2b6557c9cb57e6c356843fa62f7a5363259afe4
k1moshka <364844@gmail.com>
2014-02-16 17:19:02 +0800

    Обновлен контекст main window, добалено свойство IsSatrtWithOS, которое предоставляет одноименное свойство из настроек приложения.
    Добавленны коментарии, класс приведен к читаемому виду.

--- SymbolReflector2.0/Context.cs
+++ SymbolReflector2.0/Context.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 using System.Windows;
 using SymbolReflector.Core.Commands;
-using System.Drawing;
 
 namespace SymbolReflector
 {
@@ -20,55 +15,70 @@ namespace SymbolReflector
             get { return _cur ?? (_cur = new Context()); }
         }
         #endregion
+
         private Context() 
-        { 
+        {
+            // init dep props
             AddFilterCommand = new AddSystemFilterCommand();
             RemoveFilterCommand = new RemoveSystemFilterCommand();
             var scr = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea;
             ScreenHeight = scr.Height;
             ScreenWidth = scr.Width;
         }
-        #region commands
-
-
+        
+        /// <summary>
+        /// Показывает запускаеться ли приложение вместе с ОС. Извлекаеться из сохранненых настроек.
+        /// </summary>
+        public bool IsStartWithOS { get { return Properties.Settings.Default.IsStartWithOS; } }
+        /// <summary>
+        /// Текущая высота экрана в пикселях
+        /// </summary>
         public int ScreenHeight
         {
             get { return (int)GetValue(ScreenHeightProperty); }
             set { SetValue(ScreenHeightProperty, value); }
         }
-
-        public static readonly DependencyProperty ScreenHeightProperty =
-            DependencyProperty.Register("ScreenHeight", typeof(int), typeof(Context), new UIPropertyMetadata(480));
-
-
-
+        /// <summary>
+        /// Текущая ширина экрана в пикселях
+        /// </summary>
         public int ScreenWidth
         {
             get { return (int)GetValue(ScreenWidthProperty); }
             set { SetValue(ScreenWidthProperty, value); }
         }
 
-        public static readonly DependencyProperty ScreenWidthProperty =
-            DependencyProperty.Register("ScreenWidth", typeof(int), typeof(Context), new UIPropertyMetadata(640));
+        
 
 
-
-
+        #region commands
+        /// <summary>
+        /// 
+        /// </summary>
         public ICommand AddFilterCommand
         {
             get { return (ICommand)GetValue(AddFilterCommandProperty); }
             set { SetValue(AddFilterCommandProperty, value); }
         }
-        
+        /// <summary>
+        /// 
+        /// </summary>
         public ICommand RemoveFilterCommand
         {
             get { return (ICommand)GetValue(RemoveFilterCommandProperty); }
             set { SetValue(RemoveFilterCommandProperty, value); }
         }
+        #endregion
+
+        #region register dep props
+        public static readonly DependencyProperty ScreenWidthProperty =
+            DependencyProperty.Register("ScreenWidth", typeof(int), typeof(Context), new UIPropertyMetadata(640));
+
+        public static readonly DependencyProperty ScreenHeightProperty =
+            DependencyProperty.Register("ScreenHeight", typeof(int), typeof(Context), new UIPropertyMetadata(480));
 
         public static readonly DependencyProperty AddFilterCommandProperty =
             DependencyProperty.Register("AddFilterCommand", typeof(ICommand), typeof(Context), new UIPropertyMetadata(null));
-        
+
         public static readonly DependencyProperty RemoveFilterCommandProperty =
             DependencyProperty.Register("RemoveFilterCommand", typeof(ICommand), typeof(Context), new UIPropertyMetadata(null));
 
